e32c612b5f628781098b2c85bff438ee21fae0a5
a7meddidy <af7974943@gmail.com>
2026-01-30 14:32:20 +0200

    Add Binary Search Problem

---
+++ Week04_Binary Search & Sorting/Binary Search/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week04_Binary_Search___Sorting.Binary_Search
+{
+    public  class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            int l = 0, r = nums.Length - 1;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] < target)
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Week04_Binary Search & Sorting/Binary Search/README.md
@@ -0,0 +1,33 @@
+﻿
+
+## 📌 Binary Search – README
+
+### 🧩 Problem
+
+**Binary Search**
+Given a **sorted array** of integers and a target value, return the index of the target if it exists, otherwise return `-1`.
+
+---
+
+### 🧠 Key Idea
+
+* Sorted Array
+*  not used Linear Search o(n)
+---
+
+### 🔍 Pattern Used
+
+* **Binary Search**
+* Divide & Conquer
+* Search in sorted array
+
+---
+
+### ⏱️ Complexity
+
+* **Time:** `O(log n)`
+* **Space:** `O(1)`
+
+---
+
+
