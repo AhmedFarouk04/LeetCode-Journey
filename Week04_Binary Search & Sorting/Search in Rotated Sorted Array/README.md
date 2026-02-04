4aeb4ec1fc3c28f49f58d4594da4df455521650d
a7meddidy <af7974943@gmail.com>
2026-02-04 02:00:43 +0200

    Add Search in Rotated Sorted Array

---
+++ Week04_Binary Search & Sorting/Search in Rotated Sorted Array/README.md
@@ -0,0 +1,36 @@
+﻿Search in Rotated Sorted Array
+Problem
+
+Given a rotated sorted array with no duplicates, find the index of a target value.
+If the target does not exist, return -1.
+
+Key Idea
+
+Although the array is rotated, one half is always sorted.
+We use Binary Search and decide which half to continue searching in.
+
+Approach
+
+Use Binary Search.
+
+Check which half (left or right) is sorted.
+
+Decide if the target lies inside the sorted half.
+
+Move left or right accordingly.
+
+Time & Space Complexity
+
+Time: O(log n)
+
+Space: O(1)
+
+Example
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Pattern
+
+Binary Search
+
+Search in Rotated Sorted Array
---
+++ Week04_Binary Search & Sorting/Search in Rotated Sorted Array/Search in Rotated Sorted Array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week04_Binary_Search___Sorting.Search_in_Rotated_Sorted_Array
+{
+    public  class Search_in_Rotated_Sorted_Array
+    {
+        public int Search(int[] nums, int target)
+        {
+            int l = 0, r = nums.Length - 1;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (nums[mid] == target) return mid;
+
+                if (nums[l] <= nums[mid])
+                {
+                    if (nums[l] <= target && target < nums[mid])
+                        r = mid - 1;
+                    else
+                        l = mid + 1;
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[r])
+                        l = mid + 1;
+                    else
+                        r = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
