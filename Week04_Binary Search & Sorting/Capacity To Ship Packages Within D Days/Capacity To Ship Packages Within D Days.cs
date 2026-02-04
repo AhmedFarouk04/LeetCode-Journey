c6badeba7dad8e0ea6a81b4434ed3afef9771f08
a7meddidy <af7974943@gmail.com>
2026-02-04 22:54:39 +0200

    Add Capacity To Ship Packages Within D Days

---
+++ Week04_Binary Search & Sorting/Capacity To Ship Packages Within D Days/Capacity To Ship Packages Within D Days.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week04_Binary_Search___Sorting.Capacity_To_Ship_Packages_Within_D_Days
+{
+    public class Capacity_To_Ship_Packages_Within_D_Days
+    {
+        public int ShipWithinDays(int[] weights, int days)
+        {
+            int left = 0;
+            int right = 0;
+
+            foreach (int w in weights)
+            {
+                left = Math.Max(left, w);
+                right += w;
+            }
+
+            int answer = right;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                int daysUsed = 1;
+                int currentWeight = 0;
+
+                foreach (int w in weights)
+                {
+                    if (currentWeight + w <= mid)
+                    {
+                        currentWeight += w;
+                    }
+                    else
+                    {
+                        daysUsed++;
+                        currentWeight = w;
+                    }
+                }
+
+                if (daysUsed <= days)
+                {
+                    answer = mid;
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Week04_Binary Search & Sorting/Capacity To Ship Packages Within D Days/README.md
@@ -0,0 +1,47 @@
+﻿Ship Packages Within D Days
+Problem Idea
+
+We are given a list of package weights that must be shipped in order within a fixed number of days.
+Each day, the ship can carry packages as long as the total weight does not exceed its capacity.
+
+Our goal is to find the minimum ship capacity that allows all packages to be shipped within the given number of days.
+
+Key Observation
+
+The ship capacity cannot be smaller than the heaviest package.
+
+The ship capacity does not need to be larger than the total weight of all packages.
+
+So the answer lies within a numeric range.
+
+Approach
+
+We use Binary Search on the answer.
+
+For a guessed ship capacity:
+
+Simulate shipping day by day.
+
+Count how many days are needed.
+
+If the number of days is within the limit, try a smaller capacity.
+
+Otherwise, increase the capacity.
+
+Techniques Used
+
+Binary Search
+
+Greedy simulation
+
+Time Complexity
+
+O(n log S)
+
+n: number of packages
+
+S: range of possible capacities
+
+Final Result
+
+Binary Search helps us efficiently find the smallest valid ship capacity.
