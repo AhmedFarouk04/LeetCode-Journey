5ac990639711a3dbddb4ac8943f92a189e207ec2
a7meddidy <af7974943@gmail.com>
2026-02-05 20:37:38 +0200

    Add Magnetic Force Between Two Balls

---
+++ Week04_Binary Search & Sorting/Magnetic Force Between Two Balls/Magnetic_Force_Between_Two_Balls.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week04_Binary_Search___Sorting.Magnetic_Force_Between_Two_Balls
+{
+    public class Magnetic_Force_Between_Two_Balls
+    {
+        public int MaxDistance(int[] position, int m)
+        {
+            Array.Sort(position);
+
+            int left = 1;
+
+            int right = position[position.Length - 1] - position[0];
+
+            int answer = 0;
+
+            //  Binary Search
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (CanPlace(position, m, mid))
+                {
+                    answer = mid;
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return answer;
+        }
+
+        //Check 
+        private bool CanPlace(int[] position, int m, int dist)
+        {
+            int count = 1;                //first ball 
+            int last = position[0];       // position 
+
+            for (int i = 1; i < position.Length; i++)
+            {
+                if (position[i] - last >= dist)
+                {
+                    count++;
+                    last = position[i];
+
+                    if (count == m)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Week04_Binary Search & Sorting/Magnetic Force Between Two Balls/REEADME.md
@@ -0,0 +1,47 @@
+﻿
+---
+
+## Problem Name
+
+**Magnetic Force Between Two Balls**
+(LeetCode 1552 – Medium)
+
+---
+
+## Idea of the Solution
+
+We want to place **m balls** in given positions such that the **minimum distance between any two balls is as large as possible**.
+
+* We **binary search on the answer** (the minimum distance).
+* The smallest possible distance = `0`
+* The largest possible distance = `max(position) - min(position)`
+* For each distance `mid`, we **check if we can place all m balls** with at least this distance using a greedy approach.
+
+If we can place them:
+
+* Try a **bigger distance**
+
+If we cannot:
+
+* Try a **smaller distance**
+
+---
+
+## Technique Used
+
+* **Binary Search on Answer**
+* **Greedy check (CanPlace function)**
+
+---
+
+## Time Complexity
+
+**O(n log S)**
+
+Where:
+
+* `n` = number of positions
+* `S` = range of distances (`maxPosition - minPosition`)
+
+---
+
