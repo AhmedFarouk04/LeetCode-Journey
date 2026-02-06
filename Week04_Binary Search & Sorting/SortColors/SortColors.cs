a22c8965edfee8b544f3d8e11dc7ce157ae3eefc
a7meddidy <af7974943@gmail.com>
2026-02-06 23:31:51 +0200

    Add Sort Colors Problems

---
+++ Week04_Binary Search & Sorting/SortColors/SortColors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week04_Binary_Search___Sorting.SortColors
+{
+    public class SortColors
+    {
+        public class Solution
+        {
+            public void SortColors(int[] nums)
+            {
+                int low = 0;
+                int mid = 0;
+                int high = nums.Length - 1;
+
+                while (mid <= high)
+                {
+                    if (nums[mid] == 0)
+                    {
+                        int temp = nums[low];
+                        nums[low] = nums[mid];
+                        nums[mid] = temp;
+                        low++;
+                        mid++;
+                    }
+                    else if (nums[mid] == 1)
+                    {
+                        mid++;
+                    }
+                    else
+                    {
+                        int temp = nums[mid];
+                        nums[mid] = nums[high];
+                        nums[high] = temp;
+                        high--;
+                    }
+                }
+            }
+        }
+
+    }
+}
