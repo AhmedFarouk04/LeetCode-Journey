b04aa0a3512b051b66b7c6304e6ec231ca5a8b52
a7meddidy <af7974943@gmail.com>
2026-03-11 13:23:26 +0200

    Add Remove NTH item from the end

---
+++ Week_05Linked List/Remove NTH Item from the last/README.md
@@ -0,0 +1,16 @@
+﻿Remove Nth Node From End – LeetCode 19
+
+Idea
+
+Use Two Pointers:
+
+Move fast pointer n steps ahead.
+
+Move slow and fast together.
+
+When fast reaches the end, slow will be before the node to remove.
+
+Complexity
+
+Time: O(n)
+Space: O(1)
---
+++ Week_05Linked List/Remove NTH Item from the last/RemooveNthFromEnd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_05Linked_List.Remove_NTH_Item_from_the_last
+{
+    internal class RemooveNthFromEnd
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+
+            ListNode dummy = new ListNode(0, head);
+            ListNode slow = dummy;
+            ListNode fast = dummy;
+
+            for (int i = 0; i <= n; i++)
+                fast = fast.next;
+
+            while (fast != null)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+            slow.next = slow.next.next;
+
+            return dummy.next;
+        }
+    }
+}
