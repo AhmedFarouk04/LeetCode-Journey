de5831aa6b99fdab17f198559d1b169783ee880d
a7meddidy <af7974943@gmail.com>
2026-04-11 13:32:35 +0200

    Add avg depth Problem

---
+++ Week_06Tree/Average of Levels in Binary Tree/AverageOfLevels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Average_of_Levels_in_Binary_Tree
+{
+    internal class AverageOfLevels
+    {
+        public IList<double> Average_Of_Levels(TreeNode root)
+        {
+            var result = new List<double>();
+            if (root == null) return result;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                long sum = 0;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    sum += node.val;
+
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+
+                result.Add((double)sum / size);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Week_06Tree/Diameter of Binary Tree/DiameterOfBinaryTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Diameter_of_Binary_Tree
+{
+    public class Solution
+    {
+        private int diameter = 0;
+
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            Height(root);
+            return diameter;
+        }
+
+        private int Height(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int leftHeight = Height(node.left);
+            int rightHeight = Height(node.right);
+
+            diameter = Math.Max(diameter, leftHeight + rightHeight);
+
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
