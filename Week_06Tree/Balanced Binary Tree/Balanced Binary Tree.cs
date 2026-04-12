603d188fb67c3e7ff70ebaad30477d0dc58111a1
a7meddidy <af7974943@gmail.com>
2026-04-12 22:13:42 +0200

    Add Balanced Tree problem

---
+++ Week_06Tree/Balanced Binary Tree/Balanced Binary Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Balanced_Binary_Tree
+{
+    public class Solution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            return CheckHeight(root) != -1;
+        }
+
+        private int CheckHeight(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+
+            int leftHeight = CheckHeight(node.left);
+            if (leftHeight == -1)
+                return -1;
+
+            int rightHeight = CheckHeight(node.right);
+            if (rightHeight == -1)
+                return -1;
+
+            if (Math.Abs(leftHeight - rightHeight) > 1)
+                return -1;
+
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
---
+++ Week_06Tree/Path Sum/Path Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Path_Sum
+{
+    public class Solution
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+
+            if (root.left == null && root.right == null)
+                return targetSum == root.val;
+
+            return HasPathSum(root.left, targetSum - root.val) ||
+                   HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
