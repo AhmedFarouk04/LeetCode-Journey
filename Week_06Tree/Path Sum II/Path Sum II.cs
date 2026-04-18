0adbbbd9099905a822a61bae909d8752f8ba02d2
a7meddidy <af7974943@gmail.com>
2026-04-18 10:46:00 +0200

    Add VBST Problem

---
+++ Week_06Tree/Path Sum II/Path Sum II.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Path_Sum_II
+{
+   public  class Path_Sum_II
+    {
+
+        
+            public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+            {
+                var result = new List<IList<int>>();
+                var path = new List<int>();
+
+                Dfs(root, targetSum, path, result);
+
+                return result;
+            }
+
+            private void Dfs(TreeNode node, int targetSum, List<int> path, List<IList<int>> result)
+            {
+                if (node == null)
+                    return;
+
+                path.Add(node.val);
+
+                if (node.left == null && node.right == null && targetSum == node.val)
+                {
+                    result.Add(new List<int>(path));
+                }
+                else
+                {
+                    Dfs(node.left, targetSum - node.val, path, result);
+                    Dfs(node.right, targetSum - node.val, path, result);
+                }
+
+                path.RemoveAt(path.Count - 1); // backtrack
+            }
+        }
+    }
+
---
+++ Week_06Tree/Validate Binary Search Tree/ValidataeBST.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Validate_Binary_Search_Tree
+{
+    public  class ValidataeBST
+    {
+        
+            public bool IsValidBST(TreeNode root)
+            {
+                return Dfs(root, long.MinValue, long.MaxValue);
+            }
+
+            private bool Dfs(TreeNode node, long min, long max)
+            {
+                if (node == null) return true;
+
+                if (node.val <= min || node.val >= max)
+                    return false;
+
+                return Dfs(node.left, min, node.val) &&
+                       Dfs(node.right, node.val, max);
+            }
+        }
+    }
+
