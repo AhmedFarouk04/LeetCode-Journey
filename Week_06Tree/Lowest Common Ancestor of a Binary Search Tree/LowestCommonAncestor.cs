d55d6284d11b9e41c8c96426c2edb8982e34e384
a7meddidy <af7974943@gmail.com>
2026-04-20 11:41:29 +0200

    Add Lowest Common Ancestor of a Binary Search Tree

---
+++ Week_06Tree/Lowest Common Ancestor of a Binary Search Tree/LowestCommonAncestor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Tree.Lowest_Common_Ancestor_of_a_Binary_Search_Tree
+{
+    public  class LowestCommonAncestor
+    {
+        public class Solution
+        {
+            public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+            {
+                if (p.val < root.val && q.val < root.val)
+                    return LowestCommonAncestor(root.left, p, q);
+
+                if (p.val > root.val && q.val > root.val)
+                    return LowestCommonAncestor(root.right, p, q);
+
+                return root;
+            }
+        }
+    }
+}
