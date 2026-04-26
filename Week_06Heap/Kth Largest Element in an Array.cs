edabd723bb20e08f05b41871e1dac6dbb128fbcf
a7meddidy <af7974943@gmail.com>
2026-04-26 14:13:07 +0300

    Add Kth largest problem

---
+++ Week_06Heap/Kth Largest Element in an Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Heap
+{
+    public class Kth_Largest_Element_in_an_Array
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var pq = new PriorityQueue<int, int>();
+
+            foreach (int num in nums)
+            {
+                pq.Enqueue(num, num);
+
+                if (pq.Count > k)
+                {
+                    pq.Dequeue();
+                }
+            }
+
+            return pq.Peek();
+        }
+    }
+}
