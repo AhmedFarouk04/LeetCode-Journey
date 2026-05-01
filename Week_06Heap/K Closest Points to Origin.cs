08cfff62dbf6a919f21ed10378416db5189bf5ca
a7meddidy <af7974943@gmail.com>
2026-05-01 18:44:23 +0300

    Add K Closest Points to Origin and Find K Pairs with Smallest Sums Problems

---
+++ Week_06Heap/K Closest Points to Origin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Heap
+{
+    public  class K_Closest_Points_to_Origin
+    {
+        public class Solution
+        {
+            public int[][] KClosest(int[][] points, int k)
+            {
+                var pq = new PriorityQueue<int[], int>();
+
+                foreach (var point in points)
+                {
+                    int x = point[0];
+                    int y = point[1];
+
+                    int dist = x * x + y * y;
+
+                    pq.Enqueue(point, -dist);
+
+                    if (pq.Count > k)
+                    {
+                        pq.Dequeue();
+                    }
+                }
+
+                int[][] result = new int[k][];
+
+                for (int i = 0; i < k; i++)
+                {
+                    result[i] = pq.Dequeue();
+                }
+
+                return result;
+            }
+        }
+    }
+}
