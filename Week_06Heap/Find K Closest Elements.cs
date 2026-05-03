1767ea093d071ac835f469d88d3ed824f4971df5
a7meddidy <af7974943@gmail.com>
2026-05-03 15:36:57 +0300

    Add FindClosestElements Problems

---
+++ Week_06Heap/Find K Closest Elements.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_06Heap
+{
+    public  class Find_K_Closest_Elements
+    {
+        public class Solution
+        {
+            public IList<int> FindClosestElements(int[] arr, int k, int x)
+            {
+                var pq = new PriorityQueue<int, (int distance, int value)>(
+                    Comparer<(int distance, int value)>.Create((a, b) =>
+                    {
+                        if (a.distance != b.distance)
+                            return b.distance.CompareTo(a.distance);
+
+                        return b.value.CompareTo(a.value);
+                    })
+                );
+
+                foreach (int num in arr)
+                {
+                    int distance = Math.Abs(num - x);
+
+                    pq.Enqueue(num, (distance, num));
+
+                    if (pq.Count > k)
+                    {
+                        pq.Dequeue();
+                    }
+                }
+
+                var result = new List<int>();
+
+                while (pq.Count > 0)
+                {
+                    result.Add(pq.Dequeue());
+                }
+
+                result.Sort();
+
+                return result;
+            }
+        }
+    }
+}
