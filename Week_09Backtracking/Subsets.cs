b86c1e03f3d9d56e121bdac329913fe864663d02
a7meddidy <af7974943@gmail.com>
2026-05-16 08:17:23 +0300

    Add first backtracking problem

---
+++ Week_09Backtracking/Subsets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_09Backtracking
+{
+    public class Subsets
+    {
+        public class Solution
+        {
+            public IList<IList<int>> Subsets(int[] nums)
+            {
+                IList<IList<int>> result = new List<IList<int>>();
+
+                void Backtrack(int start, List<int> current)
+                {
+                    result.Add(new List<int>(current));
+
+                    for (int i = start; i < nums.Length; i++)
+                    {
+                        // choose
+                        current.Add(nums[i]);
+
+                        // explore
+                        Backtrack(i + 1, current);
+
+                        // unchoose
+                        current.RemoveAt(current.Count - 1);
+                    }
+                }
+
+                Backtrack(0, new List<int>());
+
+                return result;
+            }
+        }
+    }
+}
