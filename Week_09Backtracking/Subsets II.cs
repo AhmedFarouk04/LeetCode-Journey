7cd3c41151471e457a692110b27e6868d2546c59
a7meddidy <af7974943@gmail.com>
2026-05-18 02:01:02 +0300

    Add Subsets II

---
+++ Week_09Backtracking/Subsets II.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_09Backtracking
+{
+    public class Subsets_II
+    {
+        public class Solution
+        {
+            public IList<IList<int>> SubsetsWithDup(int[] nums)
+            {
+                Array.Sort(nums);
+
+                IList<IList<int>> result = new List<IList<int>>();
+
+                void Backtrack(int start, List<int> current)
+                {
+                    result.Add(new List<int>(current));
+
+                    for (int i = start; i < nums.Length; i++)
+                    {
+                        if (i > start && nums[i] == nums[i - 1])
+                            continue;
+
+                        current.Add(nums[i]);
+
+                        Backtrack(i + 1, current);
+
+                        current.RemoveAt(current.Count - 1);
+                    }
+                }
+
+                Backtrack(0, new List<int>());
+
+                return result;
+            }
+        }
+    }
+}
