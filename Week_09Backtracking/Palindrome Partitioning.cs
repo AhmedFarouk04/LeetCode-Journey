94c40b281ede75bbfdf763ce0e597cd8dd5dac4a
a7meddidy <af7974943@gmail.com>
2026-06-08 01:40:22 +0300

    Add Palindrome Partitioning  Problem

---
+++ Week_09Backtracking/Palindrome Partitioning.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_09Backtracking
+{
+    public class Palindrome_Partitioning
+    {
+        public class Solution
+        {
+            public IList<IList<string>> Partition(string s)
+            {
+                IList<IList<string>> result = new List<IList<string>>();
+                List<string> current = new List<string>();
+
+                void Backtrack(int start)
+                {
+                    if (start == s.Length)
+                    {
+                        result.Add(new List<string>(current));
+                        return;
+                    }
+
+                    for (int end = start; end < s.Length; end++)
+                    {
+                        if (!IsPalindrome(s, start, end))
+                            continue;
+
+                        string part = s.Substring(start, end - start + 1);
+
+                        current.Add(part);
+
+                        Backtrack(end + 1);
+
+                        current.RemoveAt(current.Count - 1);
+                    }
+                }
+
+                bool IsPalindrome(string text, int left, int right)
+                {
+                    while (left < right)
+                    {
+                        if (text[left] != text[right])
+                            return false;
+
+                        left++;
+                        right--;
+                    }
+
+                    return true;
+                }
+
+                Backtrack(0);
+
+                return result;
+            }
+        }
+    }
+}
