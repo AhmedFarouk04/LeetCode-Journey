5145add320f816ac0e985bee631bc1aa974200cd
a7meddidy <af7974943@gmail.com>
2026-06-13 08:55:08 +0300

    Add Climbing Stairs

---
+++ Week_10DynamicPrograming/Climbing Stairs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_10DynamicPrograming
+{
+    public class Climbing_Stairs
+    {
+        public class Solution
+        {
+            public int ClimbStairs(int n)
+            {
+                int[] memo = new int[n + 1];
+
+                return Ways(n, memo);
+            }
+
+            private int Ways(int n, int[] memo)
+            {
+                if (n == 1)
+                    return 1;
+
+                if (n == 2)
+                    return 2;
+
+                if (memo[n] != 0)
+                    return memo[n];
+
+                memo[n] = Ways(n - 1, memo) + Ways(n - 2, memo);
+
+                return memo[n];
+            }
+        }
+    }
+}
