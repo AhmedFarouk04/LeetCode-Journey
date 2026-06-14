f5251e69bce0bd18a2545c7f14145cef4c32c619
a7meddidy <af7974943@gmail.com>
2026-06-14 09:21:51 +0300

    Add House Robber  Problem

---
+++ Week_10DynamicPrograming/House Robber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_10DynamicPrograming
+{
+    public class House_Robber
+    {
+        public class Solution
+        {
+            public int Rob(int[] nums)
+            {
+                int robTwoBefore = 0;
+                int robOneBefore = 0;
+
+                foreach (int money in nums)
+                {
+                    int take = money + robTwoBefore;
+                    int skip = robOneBefore;
+
+                    int current = Math.Max(take, skip);
+
+                    robTwoBefore = robOneBefore;
+                    robOneBefore = current;
+                }
+
+                return robOneBefore;
+            }
+        }
+    }
+}
