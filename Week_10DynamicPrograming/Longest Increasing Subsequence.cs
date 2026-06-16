608c27a2111408673eddc942c6ba829862475256
a7meddidy <af7974943@gmail.com>
2026-06-16 16:02:48 +0300

    Add Longest Increasing Subsequence

---
+++ Week_10DynamicPrograming/Longest Increasing Subsequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice.Week_10DynamicPrograming
+{
+    public class Longest_Increasing_Subsequence
+    {
+        public class Solution
+        {
+            public int LengthOfLIS(int[] nums)
+            {
+                int n = nums.Length;
+
+                int[] dp = new int[n];
+
+                Array.Fill(dp, 1);
+
+                int answer = 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (nums[j] < nums[i])
+                        {
+                            dp[i] = Math.Max(dp[i], dp[j] + 1);
+                        }
+                    }
+
+                    answer = Math.Max(answer, dp[i]);
+                }
+
+                return answer;
+            }
+        }
+    }
+}
